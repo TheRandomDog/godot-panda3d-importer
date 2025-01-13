5edcf7db912ccf1e9e90ffb86e49308e987cd5da
TheRandomDog <tommeyms@gmail.com>
2025-01-13 14:21:43 -0600

    bam: Improve reading of GeomVertexArrayData
    When reading vertex data from a GeomVertexArrayData, we were mistakenly assuming two things:
    
    1) That `stride` represented the total bytes of a data array stride after reading each column. `stride` is actually the total *reserved* size for the data array, but it may not be completely used. This is (probably) done by Panda3D for efficient memory management and scaling on power-of-twos.
    
    2) That the `start` value in a data array format column was implying a column did not start until that byte in the *entire* data array for that geometry, and that as such "active columns" should be calculated per-stride. Not sure where I pulled that idea from, it's clearly not what the Panda3D documentation suggests.
    
    This commit fixes those assumptions and moves some helper functions related o the format of an array to the more-apt GeomVertexArrayFormat class. Since we're no longer trying to account for non-existent dynamic changes in the data array format, and not outputting erroneous warnings about an unexpected stride remainder, model loading/visuals should be faster and more accurate.

--- panda3d_importer/bam_objects/geom_vertex_array_data.gd
+++ panda3d_importer/bam_objects/geom_vertex_array_data.gd
@@ -6,69 +6,11 @@ var o_array_format: PandaGeomVertexArrayFormat
 var usage_hint: PandaGeom.UsageHint
 var buffer: PackedByteArray
 
-## Decodes a DirectX style color value from a uint32 (AGBR)
-func _decode_dcba() -> Color:
-	# TODO: Idk, it seems like these should be flipped?
-	var vector: Vector4 = bam_parser.decode_vector4(bam_parser.decode_u8)
-	return Color(vector.x / 255, vector.y / 255, vector.z / 255, vector.w / 255)
-
-## Decodes a DirectX style color value from a uint32 (ARGB)
-func _decode_dabc() -> Color:
-	# TODO: Idk, it seems like these should be flipped?
-	var vector: Vector4 = bam_parser.decode_vector4(bam_parser.decode_u8)
-	return Color(vector.z / 255, vector.y / 255, vector.x / 255, vector.w / 255)
-
 func parse_object_data() -> void:
 	o_array_format = bam_parser.decode_and_follow_pointer() as PandaGeomVertexArrayFormat
 	usage_hint = bam_parser.decode_u8() as PandaGeom.UsageHint
 	var buffer_size := bam_parser.decode_u32()
 	buffer = bam_parser.take_size(buffer_size)
-
-## Returns the required [Callable] from [BamParser] to decode a given
-## [enum PandaGeom.NumericType].
-func _get_decoder_for_numeric_type(numeric_type: PandaGeom.NumericType) -> Callable:
-	match numeric_type:
-		PandaGeom.NumericType.U8: return bam_parser.decode_u8
-		PandaGeom.NumericType.U16: return bam_parser.decode_u16
-		PandaGeom.NumericType.U32: return bam_parser.decode_u32
-		PandaGeom.NumericType.S8: return bam_parser.decode_s8
-		PandaGeom.NumericType.S16: return bam_parser.decode_s16
-		PandaGeom.NumericType.S32: return bam_parser.decode_s32
-		PandaGeom.NumericType.FLOAT: return bam_parser.decode_float
-		PandaGeom.NumericType.DOUBLE: return bam_parser.decode_double
-		PandaGeom.NumericType.STDFLOAT: return bam_parser.decode_stdfloat
-		PandaGeom.NumericType.PACKED_DCBA: return _decode_dcba
-		PandaGeom.NumericType.PACKED_DABC: return _decode_dabc
-		_:
-			bam_parser.parse_error('Unknown NumericType')
-			return func(): pass
-
-## Returns the number of bytes needed to read a
-## given [enum PandaGeom.NumericType].
-func _get_byte_offset_for_numeric_type(numeric_type: PandaGeom.NumericType) -> int:
-	match numeric_type:
-		PandaGeom.NumericType.U8: return 1
-		PandaGeom.NumericType.U16: return 2
-		PandaGeom.NumericType.U32: return 4
-		PandaGeom.NumericType.S8: return 1
-		PandaGeom.NumericType.S16: return 2
-		PandaGeom.NumericType.S32: return 4
-		PandaGeom.NumericType.FLOAT: return 4
-		PandaGeom.NumericType.DOUBLE: return 8
-		PandaGeom.NumericType.STDFLOAT: return 8 if bam_parser.use_f64_stdfloats else 4
-		PandaGeom.NumericType.PACKED_DCBA: return 4
-		PandaGeom.NumericType.PACKED_DABC: return 4
-		_:
-			bam_parser.parse_error('Unknown NumericType')
-			return 0
-
-## Returns how many more bytes would remain from a given stride [param remainder]
-## after reading a given [param column].
-func stride_remainder(remainder: int, column: Dictionary) -> int:
-	return remainder - (
-		column['num_components'] * 
-		_get_byte_offset_for_numeric_type(column['numeric_type'])
-	)
 
 ## Returns a [Dictionary] containing mesh data suitable for Godot's [ArrayMesh]
 ## resource. The dictionary may contain any of the following entries:
@@ -87,7 +29,6 @@ func stride_remainder(remainder: int, column: Dictionary) -> int:
 ## subset of data, any empty entries in the dictionary will be removed.
 func _gather_mesh_data() -> Dictionary:
 	var old_read_info = bam_parser.swap_read_contents(buffer)
-	var active_columns: Dictionary
 	
 	# Store a list of transformed mesh data.
 	var data = {
@@ -104,23 +45,24 @@ func _gather_mesh_data() -> Dictionary:
 		'binormals': PackedVector3Array(),
 	}
 	
-	for column in o_array_format.o_columns:
-		column['numeric_type_decoder'] = _get_decoder_for_numeric_type(column['numeric_type'])
-	
 	# We will now begin to read the byte array containing our mesh data.
 	#
 	# The o_array_format (PandaGeomVertexArrayFormat) tells us how to read the
 	# incoming byte array. In essence, the data contains multiple "columns" that
 	# are interlaced. So, if you have three columns (A, B, C), you'd take turns
 	# reading data from each of them: A -> B -> C -> A -> B -> C.
+	# Each column describes at what byte it starts at and the length to expect.
 	#
 	# This continues until the byte array is out of data. Each loop should have
-	# a set length known as a "stride", and we can use that to ensure we haven't
-	# lost our place / gotten offset in the byte array.
+	# a set length known as a "stride", which is the number of bytes reserved
+	# for that loop. The actual length of the data may be less.
+	
+	# TODO: We could likely optimize this further if we moved away from columns
+	# being dictionaries and sliced out data array strides directly to read from.
+	
+	var stride_skip := o_array_format.stride - o_array_format.total_bytes
+	
 	while bam_parser.datagram_size_remaining > 0:
-		# We keep track of "active" columns every read... theoretically it's
-		# possible for columns to start being interlaced later on, I think...
-		active_columns.clear()
 		bam_parser.ensure(
 			o_array_format.stride <= bam_parser.datagram_size_remaining,
 			("GeomVertexArrayFormat's (%s) stride is bigger than our " +
@@ -131,18 +73,12 @@ func _gather_mesh_data() -> Dictionary:
 					bam_parser.datagram_size_remaining
 				]
 		)
-		var stride_goal := bam_parser.read_byte_offset + o_array_format.stride
-		# Find our active columns this stride.
-		for column in o_array_format.o_columns:
-			# In case a new column takes precent over old one (?)
-			# TODO: This is broken anyway, column start is relative and stride_goal is cumulating the byte offset
-			if stride_goal >= column['start']:
-				active_columns[column['name'].name] = column
 		
 		# Let's start reading each column for this stride.
-		var column: Dictionary
-		for column_name in active_columns.keys():
-			column = active_columns[column_name]
+		for column in o_array_format.o_columns:
+			var alignment_goal: int = bam_parser.read_byte_offset + column['size']
+			
+			var column_name: String = column['name'].name
 			match column['contents']:
 				PandaGeom.Contents.POINT:
 					data['vertices'].append(
@@ -188,16 +124,14 @@ func _gather_mesh_data() -> Dictionary:
 				PandaGeom.Contents.COLOR:
 					data['colors'].append(column['numeric_type_decoder'].call())
 				_:
-					var unknown_size: int = _get_byte_offset_for_numeric_type(column['numeric_type']) * column['num_components']
-					push_warning('%s Skipping %s bytes of unknown column content...' % [self, unknown_size])
-					bam_parser.take_size(unknown_size)
-
-		# Sanity check
-		var remainder := stride_goal - bam_parser.read_byte_offset
-		if remainder > 0:
-			push_warning('%s Skipping %s bytes in stride remainder...' % [self, remainder])
-			bam_parser.take_size(remainder)
-
+					push_warning('%s Skipping %s bytes of unknown column content...' % [self, alignment_goal])
+					bam_parser.take_size(alignment_goal)
+					
+			if bam_parser.read_byte_offset < alignment_goal:
+				bam_parser.take_size(alignment_goal - bam_parser.read_byte_offset)
+				
+		bam_parser.take_size(stride_skip)
+		
 	bam_parser.unswap_read_contents(old_read_info)
 	
 	# We're doing reading the byte array! Let's finish up our data dictionary.
--- panda3d_importer/bam_objects/geom_vertex_array_format.gd
+++ panda3d_importer/bam_objects/geom_vertex_array_format.gd
@@ -10,6 +10,56 @@ var divisor: int = 0
 var o_columns: Array[Dictionary]
 var column_alignment: int = 1
 
+## Decodes a DirectX style color value from a uint32 (AGBR)
+func _decode_dcba() -> Color:
+	# These values work but they don't seem to match the documentation
+	var vector: Vector4 = bam_parser.decode_vector4(bam_parser.decode_u8)
+	return Color(vector.x / 255, vector.y / 255, vector.z / 255, vector.w / 255)
+
+## Decodes a DirectX style color value from a uint32 (ARGB)
+func _decode_dabc() -> Color:
+	# These values work but they don't seem to match the documentation
+	var vector: Vector4 = bam_parser.decode_vector4(bam_parser.decode_u8)
+	return Color(vector.z / 255, vector.y / 255, vector.x / 255, vector.w / 255)
+
+## Returns the required [Callable] from [BamParser] to decode a given
+## [enum PandaGeom.NumericType].
+func get_decoder_for_numeric_type(numeric_type: PandaGeom.NumericType) -> Callable:
+	match numeric_type:
+		PandaGeom.NumericType.U8: return bam_parser.decode_u8
+		PandaGeom.NumericType.U16: return bam_parser.decode_u16
+		PandaGeom.NumericType.U32: return bam_parser.decode_u32
+		PandaGeom.NumericType.S8: return bam_parser.decode_s8
+		PandaGeom.NumericType.S16: return bam_parser.decode_s16
+		PandaGeom.NumericType.S32: return bam_parser.decode_s32
+		PandaGeom.NumericType.FLOAT: return bam_parser.decode_float
+		PandaGeom.NumericType.DOUBLE: return bam_parser.decode_double
+		PandaGeom.NumericType.STDFLOAT: return bam_parser.decode_stdfloat
+		PandaGeom.NumericType.PACKED_DCBA: return _decode_dcba
+		PandaGeom.NumericType.PACKED_DABC: return _decode_dabc
+		_:
+			bam_parser.parse_error('Unknown NumericType')
+			return func(): pass
+
+## Returns the number of bytes needed to read a
+## given [enum PandaGeom.NumericType].
+func get_byte_offset_for_numeric_type(numeric_type: PandaGeom.NumericType) -> int:
+	match numeric_type:
+		PandaGeom.NumericType.U8: return 1
+		PandaGeom.NumericType.U16: return 2
+		PandaGeom.NumericType.U32: return 4
+		PandaGeom.NumericType.S8: return 1
+		PandaGeom.NumericType.S16: return 2
+		PandaGeom.NumericType.S32: return 4
+		PandaGeom.NumericType.FLOAT: return 4
+		PandaGeom.NumericType.DOUBLE: return 8
+		PandaGeom.NumericType.STDFLOAT: return 8 if bam_parser.use_f64_stdfloats else 4
+		PandaGeom.NumericType.PACKED_DCBA: return 4
+		PandaGeom.NumericType.PACKED_DABC: return 4
+		_:
+			bam_parser.parse_error('Unknown NumericType')
+			return 0
+
 func parse_object_data() -> void:
 	stride = bam_parser.decode_u16()
 	total_bytes = bam_parser.decode_u16()
@@ -18,12 +68,18 @@ func parse_object_data() -> void:
 		divisor = bam_parser.decode_u16()
 	var columns_count := bam_parser.decode_u16()
 	for i in range(columns_count):
-		o_columns.append({
+		var data := {
 			'name': bam_parser.decode_and_follow_pointer() as PandaInternalName,
 			'num_components': bam_parser.decode_u8(),
 			'numeric_type': bam_parser.decode_u8() as PandaGeom.NumericType,
 			'contents': bam_parser.decode_u8() as PandaGeom.Contents,
 			'start': bam_parser.decode_u16(),
+		}
+		data.merge({
+			'numeric_type_decoder': get_decoder_for_numeric_type(data['numeric_type']),
+			'size': data['num_components'] * get_byte_offset_for_numeric_type(data['numeric_type'])
 		})
 		if bam_parser.version >= [6, 29]:
-			o_columns[-1]['alignment'] = bam_parser.decode_u8()
+			data['alignment'] = bam_parser.decode_u8()
+		
+		o_columns.append(data)
