88ed16935ccb43c1899ef32f678fbfdc5a595cb7
TheRandomDog <tommeyms@gmail.com>
2025-02-02 11:51:55 -0600

    Cleanup texture_importer.gd

--- panda3d_importer/texture_importer.gd
+++ panda3d_importer/texture_importer.gd
@@ -42,7 +42,6 @@ func _get_preset_name(preset_index):
 func _get_import_options(path, preset_index):
 	var new_path = path.rsplit('.', true, 1)[0] + '_a.rgb'
 	var alpha_filename = ''
-	#prints(new_path, FileAccess.file_exists(new_path))
 	if FileAccess.file_exists(new_path):
 		alpha_filename = new_path
 	match preset_index:
@@ -60,18 +59,13 @@ func _get_import_options(path, preset_index):
 		_:
 			return []
 
-
 func _get_option_visibility(path, option_name, options):
 	return true
 
-func _import(source_file, save_path, options, platform_variants, gen_files) -> Error:
-	var path = source_file.rsplit('.', true, 1)
-	var file_extension = path[1]
-	
-	#var primary_file = Image.create_from_data(128, 128, false, Image.FORMAT_RGB8, primary_file_buffer)
+func _import(source_file: String, save_path, options, platform_variants, gen_files) -> Error:
 	var primary_file = Image.new()
 	var error: Error
-	match file_extension:
+	match source_file.get_extension():
 		'jpg', 'jpeg':
 			error = primary_file.load_jpg_from_buffer(FileAccess.get_file_as_bytes(source_file))
 		'png':
@@ -84,29 +78,17 @@ func _import(source_file, save_path, options, platform_variants, gen_files) -> E
 	elif primary_file.is_empty():
 		return ERR_FILE_CORRUPT
 	
+	var tex: ImageTexture
 	if options['alpha_filename']:
 		var alpha_image = load('res://' + options['alpha_filename'])
 		if alpha_image.is_empty():
 			return ERR_FILE_CORRUPT
 		
-		primary_file.convert(Image.FORMAT_RGBA8)
-		var primary_file_size := primary_file.get_size()
-		if primary_file_size != alpha_image.get_size():
-			alpha_image.resize(
-				primary_file_size.x, primary_file_size.y,
-				Image.INTERPOLATE_CUBIC
-			)
-		
-		var color: Color
-		for y in range(primary_file.get_height()):
-			for x in range(primary_file.get_width()):
-				color = primary_file.get_pixel(x, y)
-				color.a = alpha_image.get_pixel(x, y)[options['alpha_file_channel']]
-				primary_file.set_pixel(x, y, color)
-	
-	#primary_file.compress(Image.COMPRESS_BPTC)
-	var tex = ImageTexture.create_from_image(primary_file)
+		tex = Panda2Godot.merge_main_and_alpha_images(
+			primary_file, alpha_image, options['alpha_file_channel']
+		)
+	else:
+		tex = ImageTexture.create_from_image(primary_file)
 	
 	var import_filename = save_path + "." + _get_save_extension()
-	var x = ResourceSaver.save(tex, import_filename, ResourceSaver.FLAG_COMPRESS)
-	return OK
+	return ResourceSaver.save(tex, import_filename, ResourceSaver.FLAG_COMPRESS)
